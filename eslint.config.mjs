4eda3eba377412ac2027a6e54f9d6fec88f7ded0
CompuGlobalHyper <phineascrisp@gmail.com>
2026-01-07 09:54:55 -0800

    Added jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,36 @@
 import js from "@eslint/js";
-import globals from "globals";
 import css from "@eslint/css";
-import { defineConfig } from "eslint/config";
+import globals from "globals";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 
-export default defineConfig([
+export default [
+  // Base JS rules
+  js.configs.recommended,
+
+  // JavaScript files (browser + node)
   {
     files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
   },
+
+  // Jest test files
   {
-    files: ["**/*.css"],
-    plugins: { css },
-    language: "css/css",
-    extends: ["css/recommended"],
+    files: ["**/*.test.js", "**/*.spec.js"],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
   },
-  eslintConfigPrettier
-]);
+
+  // CSS files
+  css.configs.recommended,
+
+  // Disable formatting rules that conflict with Prettier
+  eslintConfigPrettier,
+];
